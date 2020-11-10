02cfbfa8d7788c0e38a4db3e87374ec4de3422be
Elvin562 <elvinbalayev86@gmail.com>
2020-11-10 15:16:27 -0500

    Talishlar oyaqdir

--- src/hi.java
+++ src/hi.java
@@ -1,5 +1,6 @@
 public class hi {
     public static void main(String[] args) {
         System.out.println("Qarabagh is Azerbaijan");
+        System.out.println("Talislar oyaqdir,Qarabagha dayaqdir!!!");
     }
 }
